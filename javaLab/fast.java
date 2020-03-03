7f855e300839f46541be765f107252c9a6917551
Your Name <you@example.com>
2020-03-03 06:17:04 +0000

    sort_update

--- javaLab/fast.java
+++ javaLab/fast.java
@@ -6,37 +6,8 @@ import java.util.Arrays;
  * fast
  */
 public class fast implements sersor{
-    int partition(int arr[], int low, int high) 
-    { 
-        int pivot = arr[high];  
-        int i = (low-1); 
-        for (int j=low; j<high; j++) 
-        { 
-            if (arr[j] < pivot) 
-            { 
-                i++; 
-                int temp = arr[i]; 
-                arr[i] = arr[j]; 
-                arr[j] = temp; 
-            } 
-        } 
-        int temp = arr[i+1]; 
-        arr[i+1] = arr[high]; 
-        arr[high] = temp; 
-  
-        return i+1; 
-    } 
-    void qsort(int arr[], int low, int high) 
-    { 
-        if (low < high) 
-        { 
-            int pi = partition(arr, low, high); 
-            qsort(arr, low, pi-1); 
-            qsort(arr, pi+1, high); 
-        } 
-    } 
     public void sort(int[] arr){
-        qsort(arr, 0, arr.length-1);
+        Arrays.sort(arr);
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
